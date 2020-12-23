92d035c991f6a4f325897ee0dcf5ae4cf0c15a9e
Bob Reselman <reselbob@gmail.com>
2020-12-23 09:39:32 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # simplemicroservice
 A project that demonstrates how to create a simple microservice that carries its own data
+
+## Installing the Code
+
+`npm install`
+
+## Testing the Code
+
+`npm test`
+
+## Running the Code
+
+`node index.js`
+
+In a second terminal windows make a call to the service:
+
+`curl localhost:3000`
+
+You get output similar to the following:
+
+`TBP`
+
+## Packaging the Code up as a Docker Container
+
+`docker build -t fortunes .`
+
+`docker container run -d -e "FORTUNE_SERVICE_PORT=3003" --name fortunes_service fortunes`
+
+## Running the Code against the Container
+
+`curl localhost:3003`
+
+You get output similar to the following:
+
+`TBP`
+
+
---
+++ index.js
@@ -0,0 +1,68 @@
+
+// call the packages we need
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const morgan = require('morgan');
+
+const {getRandomFortune} = require('./dataManager');
+
+// configure app
+app.use(morgan('dev')); // log requests to the console
+
+// configure body parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const port = process.env.FORTUNE_SERVICE_PORT || 3000; // set our port
+
+// create our router
+const router = express.Router();
+
+// middleware to use for all requests
+router.use(function (req, res, next) {
+    // do logging
+    console.log({response:res});
+    next();
+});
+
+router.route('/')
+    .get( async (req, res) => {
+        //const fortune = await getRandomFortune();
+        const fortune = await getRandomFortune();
+        const str = JSON.stringify(fortune);
+        console.log(`Sending fortune ${str} at ${new Date()}`)
+        res.send(str ).end();
+    });
+
+
+// REGISTER OUR ROUTES -------------------------------
+app.use('/', router);
+
+const server = app.listen(port, () => console.log(`Fortunes service started at ${new Date()} and listening on port ${port}`));
+const shutdown = (signal) => {
+    let shutdownMessage;
+
+    if (!signal) {
+        shutdownMessage = (`Fortunes service shutting down at ${new Date()}`);
+    } else {
+        shutdownMessage = (`Signal ${signal} : Fortunes service shutting down at ${new Date()}`);
+    }
+    const obj = {status: "SHUTDOWN", shutdownMessage, pid: process.pid};
+    await server.close(() => {
+        console.log(obj);
+        process.exit(0);
+    });
+};
+
+
+process.on("SIGTERM", () => {
+    shutdown("SIGTERM");
+});
+
+process.on("SIGINT", () => {
+    shutdown("SIGINT");
+});
+
+module.exports = {server, shutdown};
+
